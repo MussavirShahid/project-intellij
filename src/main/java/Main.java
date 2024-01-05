c6ba96e898086b0abbb1fe04c2cdd60ac10d0c4f
Muhammad Shahid <mussavirshahid@gmail.com>
2024-01-05 15:56:27 -0800

    Added Error Message

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         printMessage("Hello World");
         printMessage("My name is Muhammad");
+        System.out.println("Error");
 
     }
 
