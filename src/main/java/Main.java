fd6023d1f3a28490604c41dcef31d879fdfdd070
Muhammad Shahid <mussavirshahid@gmail.com>
2024-01-05 15:41:30 -0800

    Refactored my Main Class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("My name is Muhammad");
+        printMessage("Hello World");
+        printMessage("My name is Muhammad");
+
+    }
+
+    private static void printMessage(String message){
+        System.out.println(message);
+
+    }
+
+    private static void printHello(){
+        System.out.println("Hello");
     }
 }
